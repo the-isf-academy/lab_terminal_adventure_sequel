2b5a3ad9cc1e938ef3c1e544caaab5e15fb29bfd
GENZLINGER <bgenzlinger@TEA-BGENZLINGER.local>
2022-08-23 12:37:17 +0800

    Fix typos

--- kitchen/kitchen.py
+++ kitchen/kitchen.py
@@ -5,14 +5,14 @@ import os
 current = os.path.dirname(os.path.realpath(__file__))
 parent = os.path.dirname(current)
 sys.path.append(parent)
-  
+
 
 from helpers import print_slow
 
 def kitchen():
     print_slow("Wow this kitchen is a mess!",0.05)
     print_slow("Pirates need to learn how to organize!\n",0.05)
-    
+
     time.sleep(1)
     print_slow("*looks around*",0.2)
     time.sleep(1)
@@ -21,6 +21,6 @@ def kitchen():
     print_slow("Maybe if I bring the chef all of the ingredients, they'll make me a sandwich.",0.05)
 
 
-    print_slow("Where should I look for ingredeints?",0.05)
+    print_slow("Where should I look for ingredients?",0.05)
 
 kitchen()
